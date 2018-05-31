d5ca851b7f59c6e9d1227769633d20d0b2cebfa7
joelhooks <joelhooks@gmail.com>
2018-05-30 17:07:28 -0700

    add logo as favicon and meta details

--- pages/index.js
+++ pages/index.js
@@ -53,6 +53,13 @@ class App extends Component {
     return (
       <div>
         <Head>
+          <title>Learn Redux with Dan Abramov</title>
+          <link rel="apple-touch-icon" sizes="180x180" href="/static/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon-16x16.png" />
+          <link rel="manifest" href="/static/site.webmanifest" />
+          <link rel="mask-icon" href="/static/safari-pinned-tab.svg" color="#5bbad5" />
+          <link rel="shortcut icon" href="/static/favicon.ico" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
           <meta name="google-site-verification" content="IIrF63zU-1tJ9wTakA176fZd_hNrbW1QD_5oticuUv0" />
@@ -60,7 +67,19 @@ class App extends Component {
             name="description"
             content="Learn how to use Redux in your React apps in the tutorial from the creator of Redux, Dan Abramov."
           />
-          <title>Learn Redux with Dan Abramov</title>
+
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="msapplication-config" content="/static/browserconfig.xml" />
+          <meta name="theme-color" content="#ffffff" />
+          <meta property="og:image" content="/static/og-image.jpg" />
+          <meta property="og:image:width" content="279" />
+          <meta property="og:image:height" content="279" />
+          <meta property="og:title" content="Learn Redux for React with Dan Abramov" />
+          <meta
+            property="og:description"
+            content="Learn how to use Redux in your React apps in the tutorial from the creator of Redux, Dan Abramov."
+          />
+          <meta property="og:url" content="https://learnreduxwithdanabramov.com/" />
         </Head>
         <div
           className={css`
